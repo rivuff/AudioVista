bbb3dfc6c74a00b51730317647989970d8a914dd
Rivu Naskar <rivunaskar0@gmail.com>
2023-08-29 18:25:54 +0530

    Don't know how to fix

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from "react";
-import useSound from "use-sound";
-import { Song } from "@/types"
-import MediaItem from "./MediaItem";
-import LikeButton from "./LikeButton";
-import {BsPauseFill, BsPlayFill} from "react-icons/bs"
+import useSound from "use-sound"
+
+import { useEffect, useState } from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
-import {HiSpeakerWave, HiSpeakerXMark} from "react-icons/hi2"
-import Slider from "./Slider";
+
+import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
+
+import LikeButton from "./LikeButton";
+import MediaItem from "./MediaItem";
+import Slider from "./Slider";
 
 
 
